c715ba65fe6b5a6f27afa2187c05d0ec454f7276
Ricky Liu <rliu@spartaglobal.co>
2017-05-23 14:39:19 +0100

    first layout, default template

---
+++ main.js
@@ -0,0 +1,11 @@
+//1. add borders to stop cursor moving into it 
+
+//2.
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $('.red').click(function(){
+    	alert('HELLO!');
+    });
+});
