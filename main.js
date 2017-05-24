46057ae02c5e3bd7b1df9123bbf0d521d9feb87b
Ricky Liu <rliu@spartaglobal.co>
2017-05-24 10:11:42 +0100

    added array to make the block coloured

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-//1. add jquery 
 $(function() {
-//
+
 	// var gameInProgress = false
+		var maps = [
+			[2, 3, 7],
+			[10, 11, 12]
+		];
 
 //2.hover function to stop cursor and end game
 
     // $('.black').hover(function(){
-    // 	alert('Game Over!');
+    // 	// alert('Game Over!');
     // });
 
 //4. border to 	to stop cursor and end game
@@ -16,10 +19,41 @@ $(function() {
 	// 		gameInProgress = true
 	// 	} else {
 	// 		alert('Game Over');
-
-	// 	}
-
-
+	// }
 	// });
 
+//5.alert when game is completed
+
+// $('#h').hover(function(){
+// 	alert('Well done');
+// });
+
+
+//6.make the block randomise
+
+//7. menu button with start function
+
+	$('#d').click(function(event){
+	alert("start Game");
 });
+//8. Coloured blocks to be radomised once you enter new level
+
+	var currentMap =["#2","#3","#7"]
+
+	for (var i = 0; i < currentMap.length; i++){
+	$(currentMap[i]).addClass('black');
+
+	};
+
+$('#next').click(function(event){
+	$('maps').append('#'+ currentMap[i]);
+
+});
+
+
+
+
+});
+
+
+// });
